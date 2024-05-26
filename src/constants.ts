48122211c678f06db933a0fd514004a16b01f63a
Vigor Akbar <vigorakbar@gmail.com>
2024-05-26 23:19:11 +0800

    fix: fix blocking background

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const HOUR_IN_MS = MINUTE_IN_MS * 60;
 export const DAY_IN_MS = HOUR_IN_MS * 24;
 
 export const ASTON_GMAPS_LINK = 'https://maps.app.goo.gl/62up4dgdyENUnu8o7';
-const title = 'Aira & Vigor | The Wedding';
+const title = "Aira & Vigor's Wedding";
 const details =
   'With immense joy in our hearts, we invite you to witness and celebrate the next chapter of our love story as we embark on this journey together, towards forever';
 const dates = '20240914T120000/20240914T150000';
