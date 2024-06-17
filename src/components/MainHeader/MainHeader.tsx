0fb132abc94d774f81320f08e77a31b9abb2ed2d
Vigor Akbar <vigorakbar@gmail.com>
2024-06-17 22:27:36 +0800

    feat: add scroll down hint

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,10 +1,12 @@
+import doubledown from '../../assets/images/double-down.png';
 import { useScrollAreaProgress } from '../../hooks/useScrollProgress';
 import s from './MainHeader.module.scss';
 import { MainCard } from './components/MainCard/MainCard';
 import { OpeningInvitation } from './components/OpeningInvitation/OpeningInvitation';
 import Aos from 'aos';
+import classNames from 'classnames';
 import debounce from 'lodash.debounce';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const MainHeader = ({
   setMainHeaderFinished,
@@ -15,6 +17,7 @@ export const MainHeader = ({
 }) => {
   const { progress, scrollAreaRef } = useScrollAreaProgress();
   const debouncedFinished = debounce(setMainHeaderFinished, 200);
+  const [showHint, setShowHint] = useState(false);
 
   useEffect(() => {
     if (progress >= 0.85) {
@@ -28,12 +31,33 @@ export const MainHeader = ({
     Aos.refresh();
   }, [mainHeaderFinished]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setShowHint(true);
+    }, 500);
+  }, []);
+
+  console.log(progress);
+
   return (
     <div className={s.scrollArea} ref={scrollAreaRef}>
       <div className={s.mainHeaderContainer}>
         <div className={s.innerContainer}>
           <MainCard progress={progress} />
         </div>
+        {progress < 0.5 && (
+          <div
+            className={classNames(
+              s.scrollDownHint,
+              showHint && progress < 0.2 && s.showHint,
+            )}
+          >
+            <div>Scroll ke bawah</div>
+            <div>
+              <img src={doubledown} />
+            </div>
+          </div>
+        )}
       </div>
       {mainHeaderFinished && <OpeningInvitation />}
     </div>
