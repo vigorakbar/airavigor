d0a8329a51043d7e32a66005fc099f46d8bc3a9b
Vigor Akbar <vigor.akbar@shopee.com>
2024-07-30 14:54:26 +0800

    feat: update initial image load

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import s from './App.module.scss';
+import airaVigorHalimun from './assets/images/airavigor-halimun.png';
+import avLejour from './assets/images/av-lejour.png';
 import envelbackenv from './assets/images/envel-backenv.png';
 import envelbacklid from './assets/images/envel-backlid.png';
 import envelfront from './assets/images/envel-front.png';
 import envellid from './assets/images/envel-lid.png';
-// import { CardMainContent } from './components/CardMainContent/CardMainContent';
+import frontframe from './assets/images/frontframe.png';
+import paperBackground from './assets/images/paper-background.png';
 import { EnvelopeSection } from './components/Envelope/EnvelopeSection';
 import { MainHeader } from './components/MainHeader/MainHeader';
 import { Separator } from './components/Separator/Separator';
@@ -24,7 +27,15 @@ import cx from 'classnames';
 import React, { useEffect, useRef, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 
-const imagesNeedLoad = [envelbackenv, envelbacklid, envelfront, envellid];
+const imagesNeedLoad = [
+  envelbackenv,
+  envelbacklid,
+  envelfront,
+  envellid,
+  airaVigorHalimun,
+  avLejour,
+  frontframe,
+];
 
 const App: React.FC = () => {
   const [envelopeOpened, setEnvelopeOpened] = useState(false);
@@ -38,9 +49,11 @@ const App: React.FC = () => {
 
   useEffect(() => {
     const loadImages = async () => {
-      const promises = imagesNeedLoad.map(imgUrl => loadRemoteImage(imgUrl));
+      const imagesPromise = imagesNeedLoad.map(imgUrl =>
+        loadRemoteImage(imgUrl),
+      );
       try {
-        await Promise.all(promises);
+        await Promise.all(imagesPromise);
       } catch (e) {
         setLoadingImg(false);
       }
@@ -48,6 +61,7 @@ const App: React.FC = () => {
       setLoadingImg(false);
     };
 
+    loadRemoteImage(paperBackground);
     loadImages();
   }, []);
 
