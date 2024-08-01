39fd390131272253e35b3226439e18a9c25066dd
Vigor Akbar <vigorakbar@gmail.com>
2024-08-01 22:17:51 +0800

    feat: add flower to bride and groom frame

--- src/components/contents/BrideAndGroom/BrideAndGroom.tsx
+++ src/components/contents/BrideAndGroom/BrideAndGroom.tsx
@@ -1,4 +1,6 @@
 import airalejour from '../../../assets/images/aira-halimun.png';
+import framePurpleBig from '../../../assets/images/framePurpleBig.png';
+import framePurpleSmall from '../../../assets/images/framePurpleSmall.png';
 import photoframe from '../../../assets/images/photoframe.png';
 import sample from '../../../assets/images/sample.png';
 import vigorlejour from '../../../assets/images/vigor-halimun.png';
@@ -6,34 +8,47 @@ import { FrameBase } from '../../Frame/FrameBase';
 import { Title } from '../../Title/Title';
 // import { NameText } from '../../NameText/NameText';
 import s from './BrideAndGroom.module.scss';
+import classNames from 'classnames';
 import React from 'react';
 
 type Props = {
   name: 'aira' | 'vigor';
 };
 
-export const BrideAndGroom: React.FC<Props> = ({ name }) => (
-  <div className={s.container}>
-    {name === 'aira' ? <Title>The Bride and Groom</Title> : ' '}
-    <FrameBase className={s.frameBase}>
-      <img src={photoframe} className={s.photoframe} />
-      {/* TODO: use actual photo (dimension approx. 316 x 471 ) */}
-      <div className={s.photoWrapper}>
-        <img src={sample} className={s.photo} />
+export const BrideAndGroom: React.FC<Props> = ({ name }) => {
+  const isAira = name === 'aira';
+  return (
+    <div className={s.container}>
+      {isAira ? <Title>The Bride and Groom</Title> : ' '}
+      <FrameBase className={s.frameBase}>
+        <img
+          src={framePurpleBig}
+          className={classNames(s.framePurpleBig, isAira ? s.aira : s.vigor)}
+        />
+        <img
+          src={framePurpleSmall}
+          className={classNames(s.framePurpleSmall, isAira ? s.aira : s.vigor)}
+        />
+
+        <img src={photoframe} className={s.photoframe} />
+        {/* TODO: use actual photo (dimension approx. 316 x 471 ) */}
+        <div className={s.photoWrapper}>
+          <img src={sample} className={s.photo} />
+        </div>
+      </FrameBase>
+      {/* <NameText>{isAira ? 'Aira' : 'Vigor'}</NameText> */}
+      {isAira ? (
+        <img src={airalejour} className={s.name} />
+      ) : (
+        <img src={vigorlejour} className={s.name} />
+      )}
+      <div>{isAira ? 'Airadiba Hadad' : 'Vigor Akbar'}</div>
+      <div>{isAira ? 'Putri' : 'Putra'} kedua dari</div>
+      <div>
+        {isAira
+          ? 'Bapak Ashif Hadad & Ibu Yeni Heryani'
+          : 'Bapak Dwiyarik Hafid Hidayat & Ibu Ratna Ningrum'}
       </div>
-    </FrameBase>
-    {/* <NameText>{name === 'aira' ? 'Aira' : 'Vigor'}</NameText> */}
-    {name === 'aira' ? (
-      <img src={airalejour} className={s.name} />
-    ) : (
-      <img src={vigorlejour} className={s.name} />
-    )}
-    <div>{name === 'aira' ? 'Airadiba Hadad' : 'Vigor Akbar'}</div>
-    <div>{name === 'aira' ? 'Putri' : 'Putra'} kedua dari</div>
-    <div>
-      {name === 'aira'
-        ? 'Bapak Ashif Hadad & Ibu Yeni Heryani'
-        : 'Bapak Dwiyarik Hafid Hidayat & Ibu Ratna Ningrum'}
     </div>
-  </div>
-);
+  );
+};
