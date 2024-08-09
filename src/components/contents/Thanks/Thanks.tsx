0b2ea0590d0df405719408f8766623f80b76f04d
Vigor Akbar <vigorakbar@gmail.com>
2024-08-10 00:35:02 +0800

    feat: thanks section

--- src/components/contents/Thanks/Thanks.tsx
+++ src/components/contents/Thanks/Thanks.tsx
@@ -1,9 +1,25 @@
 import { SectionContainer } from '../../SectionContainer/SectionContainer';
-import { Title } from '../../Title/Title';
 import s from './Thanks.module.scss';
+
+const AOS_ANCHOR = {
+  ['data-aos-anchor-placement']: 'bottom-bottom',
+  ['data-aos-anchor']: '#thanks-section',
+  ['data-aos-duration']: 900,
+  ['data-aos-delay']: 500,
+};
 
 export const Thanks = () => (
   <SectionContainer className={s.container}>
-    <Title className={s.title}>Thank You</Title>
+    <div id="thanks-section">
+      <div data-aos="fade-down" {...AOS_ANCHOR}>
+        The wedding of
+      </div>
+      <div className={s.name} data-aos="zoom-in" {...AOS_ANCHOR}>
+        <span>Aira</span> & <span>Vigor</span>
+      </div>
+      <div data-aos="fade-up" {...AOS_ANCHOR}>
+        Saturday, September 14th 2024
+      </div>
+    </div>
   </SectionContainer>
 );
